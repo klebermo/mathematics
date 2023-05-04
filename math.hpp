8481c3ca860f512e88677d5f8ef6462443f4d25b
Kleber Mota de Oliveira <klebermo@gmail.com>
2023-05-04 18:54:51 -0300

    Update 04/05/2023 18:54

--- math.hpp
+++ math.hpp
@@ -1,39 +1,23 @@
 #ifndef MATH_H
 #define MATH_H
 
-namespace number_theory {
-    #include "number_theory/number_theory.hpp"
-}
+#include "number_theory/number_theory.hpp"
 
-namespace geometry {
-    #include "geometry/geometry.hpp"
-}
+#include "geometry/geometry.hpp"
 
-namespace algebra {
-    #include "algebra/algebra.hpp"
-}
+#include "algebra/algebra.hpp"
 
-namespace calculus_and_analysis {
-    #include "calculus/calculus.hpp"
-    #include "calculus/analysis.hpp"
-}
+#include "calculus/calculus.hpp"
+#include "calculus/analysis.hpp"
 
-namespace discrete_mathetamatics {
-    #include "discrete_math/discrete.hpp"
-}
+#include "discrete_math/discrete.hpp"
 
-namespace logic_and_set_theory {
-    #include "logic/logic.hpp"
-    #include "logic/set_theory.hpp"
-}
+#include "logic/logic.hpp"
+#include "logic/set_theory.hpp"
 
-namespace probability {
-    #include "probability/probability.hpp"
-}
+#include "probability/probability.hpp"
 
-namespace statistics_and_decision_theory {
-    #include "statistics/statistics.hpp"
-    #include "statistics/decision_theory.hpp"
-}
+#include "statistics/statistics.hpp"
+#include "statistics/decision_theory.hpp"
 
 #endif
